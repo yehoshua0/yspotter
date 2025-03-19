4e973044afb0f1e42aed150dfc461eb7ad4329fa
Kodjo Josué AYITEY <jackjosue517@gmail.com>
2025-03-19 12:38:19 +0100

    Refreshing the project

--- code/backend/backend/settings.py
+++ code/backend/backend/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://yspotter.vercel.com",
+    "https://yspotter.vercel.app",
 ]
