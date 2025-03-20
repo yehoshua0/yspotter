19ee551c2073673dc0cf5daf7ea5e5bfde1cda9a
Kodjo Josué AYITEY <jackjosue517@gmail.com>
2025-03-20 21:31:03 +0100

    Modify settings.py in backend (DATABASE CONFIG)

---
+++ code/frontend/src/components/widgets/DailyLog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DailyLog = () => {
+  return (
+    <div>
+      <h1>Daily Log</h1>
+      <p>This is the Daily Log widget.</p>
+    </div>
+  )
+}
+
+export default DailyLog
---
+++ code/frontend/src/components/widgets/Route.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RouteWidget = () => {
+  return <h1>Route</h1>
+}
+
+export default RouteWidget
