9229463a192652bd86b4c47fa37fac3ae34e640c
yehoshua0 <jackjosue517@gmail.com>
2025-04-08 13:57:51 +0200

    Config Vercel url handling settings

--- code/frontend/src/constants.js
+++ code/frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const ACCESS_TOKEN = "access"
-export const REFRESH_TOKEN = "refresh"
-export const USERNAME = "user"
+export const ACCESS_TOKEN = 'access'
+export const REFRESH_TOKEN = 'refresh'
+export const USERNAME = 'user'
